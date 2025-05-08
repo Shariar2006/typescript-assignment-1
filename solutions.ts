6aea7559ea49ec89c8d04666022d69be3a1c53f6
Shariar2006 <shariarnafis86@gmail.com>
2025-05-08 06:46:56 +0600

    solve problem 2

--- solutions.ts
+++ solutions.ts
@@ -1,14 +1,29 @@
 {
 
-    function formatString(input: string, toUpper: boolean = true): string{
-        if(!toUpper){
+    function formatString(input: string, toUpper: boolean = true): string {
+        if (!toUpper) {
             return input?.toLowerCase()
-        }else{
+        } else {
             return input?.toUpperCase()
         }
     }
 
-    const stringFormat = formatString("ljlfkjd")
+    const stringFormat = formatString("Hello")
 
-console.log(stringFormat)
+    // console.log(stringFormat)
+
+
+    function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+       return items?.filter(item => item?.rating >= 4)
+    }
+
+    const books = [
+        { title: "Book A", rating: 4.5 },
+        { title: "Book B", rating: 3.9 },
+        { title: "Book C", rating: 5.0 }
+    ];
+
+    const filterBooks = filterByRating(books);
+    // console.log(filterBooks)
+
 }
